cc2741b43eeb72d9c70666236e73bc7fecdbbac4
o0Pleiades0o <redkarroy@gmail.com>
2025-02-03 12:29:49 +0700

    1

--- router-react/src/App.jsx
+++ router-react/src/App.jsx
@@ -6,13 +6,31 @@ import Product from "./components/pages/Product.jsx"
 import Header from "./components/layouts/Header.jsx"
 
 function App() {
+  let products = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 100
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 200
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 300
+    }
+  ]
+
   return (
     <>
       <Header />
       <Routes>
         <Route index element={<Home />} />
         <Route path="/About" element={<About />} />
-        <Route path="/P/:id" element={<Product />} />
+        <Route path="/P/:id" element={<Product products={products} />} />
       </Routes>
     </>
   )
--- router-react/src/components/pages/Product.jsx
+++ router-react/src/components/pages/Product.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useParams } from 'react-router'
-
-const Product = () => {
+const Product = (props) => {
 let params = useParams()
+let products = props.products.find(item => item.id == params.id) === 
+undefined ? {name : "Not Found", price : "Not Found"} : props.products.find(item => item.id == params.id)
   return (
     <div>
-      Product :id {params.id}
+      Product ID : {params.id} <br />
+      Product Name : {products.name} <br />
+      Product Price : {products.price}  
     </div>
   )
 }
