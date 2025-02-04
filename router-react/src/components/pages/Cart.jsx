742225fe0eecfc9ff4f80bfba70d8be0b45ec67c
o0Pleiades0o <redkarroy@gmail.com>
2025-02-04 19:48:48 +0700

    3

--- router-react/src/components/pages/Cart.jsx
+++ router-react/src/components/pages/Cart.jsx
@@ -42,8 +42,8 @@ const Cart = () => {
         <div className="col-md-8">
           {cartItems.map((item) => (
             <div key={item.Name} className="card mb-3">
-              <div className="card-body d-flex justify-content-between align-items-center">
-                <div className="d-flex align-items-center">
+              <div className="card-body d-flex justify-content-between">
+                <div className="d-flex">
                   <img
                   className='rounded'
                     src={item.image}
